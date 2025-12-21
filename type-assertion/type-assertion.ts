64da13603ad3354516efada66ac391a1ca35e48c
katz16 <karthikkumar654@gmail.com>
2025-12-21 11:40:45 +0530

    type assertion

---
+++ type-assertion/type-assertion.ts
@@ -0,0 +1,5 @@
+const element=document.getElementById("my_element") as HTMLInputElement;
+element.value="Hello world";
+
+// the as keyword strongly defines the element as HTMLInputElement.
+// as keyword is used for type assertion 
